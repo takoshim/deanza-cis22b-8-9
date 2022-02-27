383c7058a945b5ffcebcb4828192d148e14757f3
tako2039 <takeo2039@gmail.com>
2022-02-27 15:24:33 -0600

    include <string> in Student.h

--- Student.h
+++ Student.h
@@ -10,23 +10,22 @@
 // namespace management problems for the entire project
 // (such as name collisions).
 // Do not write namespace using statements at the top level in a header file!
-
-using std::string;
+#include <string>
 
 class Student
 {
 private:
     double gpa;
-    string name;
+    std::string name;
     
 public:
     Student() {name = ""; gpa = -1;}  // Constructor
-    Student(double g, string n) {name = n; gpa = g;}  // Overloaded Constructor
+    Student(double g, std::string n) {name = n; gpa = g;}  // Overloaded Constructor
     
     // Setters and getters
-    void setName(string n) {name = n;}
+    void setName(std::string n) {name = n;}
     void setGpa(double g) {gpa = g;}
-    string getName() const {return name;}
+    std::string getName() const {return name;}
     double getGpa() const {return gpa;}
     
 };
